d2626a319a1cceb7c92c2733273333e49c65192f
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-06-04 00:18:25 -0400

    latest projects section done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,22 @@
 const Projects = (props) => {
     return (
-        <div>
-            
+        <div className="singleProjectSection">
+            <div className="singleProject">
+                <span>{props.title}</span>
+                <img className="projectImg" src={props.image} alt={props.imageAlt} />
+                <p className ="projectDescription">{props.description}</p>
+                <div className="iconProject">
+                    <a href={props.heroku} target="_blank" rel="noreferrer">
+                    <img className="projectIconImg" src="https://icongr.am/entypo/browser.svg?color=4d4d4d" alt="linkedin icon" />
+                    </a>
+                    <a href={props.github} target="_blank" rel="noreferrer">
+                    <img src="https://icongr.am/devicon/github-original.svg" alt="github icon" />
+                    </a>
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -1,7 +1,37 @@
+import Projects from "../components/Projects"
+
+
 const AllProjects = () => {
     return (
-        <div className="projectSection">
-           <h3>Latest Projects</h3>
+        <div className="projectsSection">
+            <div>
+                <h3 className="projectsHeader">Latest Projects</h3>
+            </div>
+           
+           <div className="projectsList">
+               <Projects                     
+                    image="https://i.imgur.com/JgrEsFp.png"
+                    imageAlt="handmade"
+                    title="Handmade"
+                    description="An e-commerce website that allows users to buy exclusive, unique and handmade ONLY products. Built using JavaScript/Node/API/Express/PostgreSQL"
+                    heroku="https://handmade-frontend.herokuapp.com/"
+                    github="https://github.com/aneltuyebayeva/handmade-frontend" />
+            
+                <Projects                     
+                    image="https://i.imgur.com/d7cboW3.png"
+                    imageAlt="partylist"
+                    title="Partylist"
+                    description="This app contains information about any party providers that people need to plan their events and parties. Built using React/Node/Express/PostgreSQL"
+                    heroku="https://partylistfrontend.herokuapp.com/"
+                    github="https://github.com/aneltuyebayeva/partylist-frontend" />
+                <Projects                     
+                    image="https://i.imgur.com/IO1pOqc.png"
+                    imageAlt="travellovers"
+                    title="Travel Lovers"
+                    description="This app allows travellers to plan ahead their travel and see a list of top sightseeing places in any city in the world. Users are able to save places they would want to visit. Built using JavaScript/Node/API/Express/PostgreSQL"
+                    heroku="https://travel-loverss.herokuapp.com/"
+                    github="https://github.com/aneltuyebayeva/travel-lovers-frontend" />
+           </div>
         </div>
     )
 }
